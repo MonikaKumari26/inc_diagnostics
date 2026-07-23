579b452cecc7fd26e1679f550968817ab604be38
MonikaKumari26 <Monika.Kumari@bti.bmwgroup.com>
2026-07-23 16:56:45 +0530

    replace deprecated score::cpp::blank with Result<void>

--- score/mw/diag/diag_result.h
+++ score/mw/diag/diag_result.h
@@ -21,9 +21,6 @@
 #ifndef SCORE_MW_DIAG_DIAG_RESULT_H
 #define SCORE_MW_DIAG_DIAG_RESULT_H
 
-#include "score/mw/diag/byte_types.h"
-#include "score/mw/diag/uds/negative_response_code.h"
-
 #include "score/result/result.h"
 
 namespace score::mw::diag::uds
